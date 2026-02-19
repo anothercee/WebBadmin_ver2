0ae09faec27c87d68ed2705fe7f86c38d0b7c42f
anothercee <anothersideofcee17@gmail.com>
2026-02-19 09:40:08 +0700

    Update aurora.js

--- aurora.js
+++ aurora.js
@@ -50,10 +50,10 @@ float noise(vec2 st){
 void main(){
   vec2 uv = gl_FragCoord.xy / uRes;
   
-  float n = noise(vec2(uv.x*3.0, uTime*0.4));
-  float y = uv.y + n*0.3;
+  float n = noise(vec2(uv.x*5.0, uTime*0.4));
+  float y = uv.y + n*0.9 - 0.5;
 
-  float intensity = smoothstep(0.4,0.8,y);
+  float intensity = smoothstep(0.3,0.6,y);
 
   vec3 color1 = vec3(0.5,0.0,1.0);   
   vec3 color2 = vec3(0.2,0.0,0.6);   
